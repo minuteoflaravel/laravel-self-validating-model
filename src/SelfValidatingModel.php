39b4f7d8b88746a2e7389c7ac79f6c893ac207c4
Minute Of Laravel <contact@minuteoflaravel.com>
2022-05-12 23:08:50 +0300

    first commit

---
+++ src/SelfValidatingModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MinuteOfLaravel\SelfValidatingModel;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+
+class SelfValidatingModel extends Model {
+    public $rules = [];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            Validator::make($model->toArray(), $model->rules)->validate();
+        });
+    }
+}
